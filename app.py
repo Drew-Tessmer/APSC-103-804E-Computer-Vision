9b729b66698470d00d8fd2db1c629e3a127229f0
Francesca Adaobi <francesca7704@gmail.com>
2024-03-19 21:37:22 -0400

    Login draft

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-
+# Registered person
+person =[]
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
+@app.route("/registrants")
+def registrants():
+    return render_template("registered.html", person=person)
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    isMemberof = request.form.get("isMemberof")
+    if not name or not isMemberof:
+        return render_template("failure.html")
+    else:
+        person.append(f"{name} from {isMemberof}")
+        return redirect("/registrants")
 
 if __name__ == '__main__':
     app.run()
