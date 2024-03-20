cdfb65c2e606101fbe9888140845dee38ea35680
santosrojella <santosrojella@gmail.com>
2024-03-20 13:35:14 -0400

    newest
    cjhvkjblkl

--- Running_Yolov8/Yolo_test.py
+++ Running_Yolov8/Yolo_test.py
@@ -4,8 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data="config.yaml", epochs=3)  # train the model
-
+model.train(data="config.yaml", epochs=1)  # train the model
 
 # metrics = model.val()  # evaluate model performance on the validation set
 # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
@@ -13,7 +12,7 @@ model.train(data="config.yaml", epochs=3)  # train the model
 
 
 
-import cv2
+# import cv2
 #model=YOLO('yolov8n.pt') this uses a PreTrained model
-result=model("../")
-cv2.waitkey(0)
+# result=model("../")
+# cv2.waitkey(0)
