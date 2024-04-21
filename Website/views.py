5b9da5570cd81eb7e823b5cce013f707bfff7db9
Drew-Tessmer <drewtessmer20@gmail.com>
2024-04-21 17:22:52 -0400

    Updated web design

--- Website/views.py
+++ Website/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+import os
 
 views = Blueprint('views', __name__)
 bp = Blueprint('my_blueprint', __name__, static_folder='static', template_folder='templates')
@@ -6,3 +7,15 @@ bp = Blueprint('my_blueprint', __name__, static_folder='static', template_folder
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+
+    # Ensure that the 'uploads' directory exists
+    os.makedirs('uploads', exist_ok=True)
+
+    # Save the file with its original name in the 'uploads' directory
+    file.save(os.path.join('uploads', file.filename))
+
+    return redirect('/')
