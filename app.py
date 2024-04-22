09a777433e1822f00f3260642764aa7686a749c2
Francesca Adaobi <francesca7704@gmail.com>
2024-04-22 11:12:02 -0400

    "Amend"

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
 from Website.__init__ import create_app
 from flask import Flask, render_template, jsonify, request,redirect
-
-app = Flask(__name__)
-
-# Registered person
-person =[]
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route("/registrants")
-def registrants():
-    return render_template("registered.html", person=person)
-@app.route("/register", methods=["POST"])
-def register():
-    name = request.form.get("name")
-    isMemberof = request.form.get("isMemberof")
-    if not name or not isMemberof:
-        return render_template("failure.html")
-    else:
-        person.append(f"{name} from {isMemberof}")
-        return redirect("/registrants")
-
-
+import os
+import cv2
 
 # Create Flask app instance
 app = create_app()
